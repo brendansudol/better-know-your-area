c9266561980d3b07c6a474d9f73b2f8d7eb5ce27
brendan sudol <brendansudol@gmail.com>
2017-12-21 21:42:24 -0500

    metrics by category

--- src/components/App.js
+++ src/components/App.js
@@ -4,12 +4,11 @@ import VirtualizedSelect from 'react-virtualized-select'
 import Footer from './Footer'
 import Header from './Header'
 import Loading from './Loading'
-import NumberAnimate from './NumberAnimate'
 import Map from './Map'
 
 import { CATEGORIES, METRICS } from '../util/metrics'
 import { computeDiff } from '../util/misc'
-import { formatNum, formatPerc } from '../util/formats'
+import { fmt, formatNum, formatPerc } from '../util/formats'
 
 class App extends Component {
   constructor(props) {
@@ -49,6 +48,15 @@ class App extends Component {
     if (data.length === 0) return <Loading />
     if (!datum) return <p>BAD GEOID (TODO: BETTER UI)</p>
 
+    const selectOptions = data
+      .filter(d => d.sumlevel === '050')
+      .map(d => ({ label: d.name, value: d.geoid }))
+
+    const catOptions = ['All', ...CATEGORIES].map(c => ({
+      id: c.toLowerCase(),
+      display: c,
+    }))
+
     const { name, population, related, metrics } = datum
     const state = data.find(d => d.geoid === related.state)
     const usa = data.find(d => d.geoid === '01000US')
@@ -66,16 +74,15 @@ class App extends Component {
         state: { val: valState, diff: computeDiff(val, valState) },
         usa: { val: valUsa, diff: computeDiff(val, valUsa) },
       }
-    }).filter(m => (cat !== 'all' ? m.catLower === cat : true))
+    })
 
-    const selectOptions = data
-      .filter(d => d.sumlevel === '050')
-      .map(d => ({ label: d.name, value: d.geoid }))
-
-    const catOptions = ['All', ...CATEGORIES].map(c => ({
-      id: c.toLowerCase(),
-      display: c,
-    }))
+    const metricsByCat = catOptions
+      .slice(1)
+      .map(cat => ({
+        cat,
+        metrics: metricsData.filter(m => m.catLower === cat.id),
+      }))
+      .filter(d => (cat !== 'all' ? d.cat.id === cat : true))
 
     return (
       <div className="p2">
@@ -89,9 +96,8 @@ class App extends Component {
           />
         </div>
 
-        <div className="mb3 h3 monospace bold">
-          <NumberAnimate value={metrics.median_house_value} />
-        </div>
+        <h2>{name}</h2>
+        <div className="mb3">Population: {formatNum(population)}</div>
 
         <div className="mb2">
           {catOptions.map(c => (
@@ -111,71 +117,42 @@ class App extends Component {
         {false && <Map data={data} geoid={geoid} />}
 
         <div className="mb2">
-          <h2>{name}</h2>
-          <div className="mb1">Population: {formatNum(population)}</div>
+          {metricsByCat.map(({ cat, metrics }) => (
+            <div key={cat.id} className="mb2">
+              <h3>{cat.display}</h3>
 
-          {metricsData.map(d => (
-            <div key={d.id} className="mb1 py1 border-bottom border-silver">
-              <div>{d.name}</div>
-              <div className="h3 monospace clearfix">
-                <div className="sm-col sm-col-3">
-                  <NumberAnimate value={d.val} format={d.fmt} />
-                </div>
-                <div className="sm-col sm-col-3">
-                  {formatPerc(d.state.diff)}
-                </div>
-                <div className="sm-col sm-col-3">{formatPerc(d.usa.diff)}</div>
-                <div className="sm-col sm-col-3">
-                  <progress
-                    value="0.375"
-                    className="progress"
-                    style={{ maxWidth: '80%' }}
-                  >
-                    0.375
-                  </progress>
-                </div>
-              </div>
+              <table className="col-12 bg-white">
+                <thead className="left-align">
+                  <tr>
+                    <th className="col-4" />
+                    <th className="col-2">Metric</th>
+                    <th className="col-2">vs. State</th>
+                    <th className="col-2">vs. USA</th>
+                    <th className="col-2">Rank</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {metrics.map(m => (
+                    <tr key={m.id}>
+                      <td>{m.name}</td>
+                      <td className="monospace">{fmt(m.val, m.fmt)}</td>
+                      <td className="monospace">{formatPerc(m.state.diff)}</td>
+                      <td className="monospace">{formatPerc(m.usa.diff)}</td>
+                      <td>
+                        <progress
+                          value="0.375"
+                          className="progress"
+                          style={{ maxWidth: '80%' }}
+                        >
+                          0.375
+                        </progress>
+                      </td>
+                    </tr>
+                  ))}
+                </tbody>
+              </table>
             </div>
           ))}
-
-          {metricsData.map(d => (
-            <div key={d.id} className="mb1 p1 bg-white clearfix">
-              <div className="sm-col sm-col-4">{d.name}</div>
-              <div className="sm-col sm-col-2">{d.val}</div>
-              <div className="sm-col sm-col-2">{formatPerc(d.state.diff)}</div>
-              <div className="sm-col sm-col-2">{formatPerc(d.usa.diff)}</div>
-              <div className="sm-col sm-col-2">
-                <progress
-                  value="0.375"
-                  className="progress"
-                  style={{ maxWidth: '80%' }}
-                >
-                  0.375
-                </progress>
-              </div>
-            </div>
-          ))}
-
-          <table>
-            <thead className="left-align">
-              <tr>
-                <th>Metric</th>
-                <th>Value</th>
-                <th>Compared with State</th>
-                <th>Compared with USA</th>
-              </tr>
-            </thead>
-            <tbody>
-              {metricsData.map(d => (
-                <tr key={d.id}>
-                  <td>{d.name}</td>
-                  <td>{d.val}</td>
-                  <td>{d.state.diff}</td>
-                  <td>{d.usa.diff}</td>
-                </tr>
-              ))}
-            </tbody>
-          </table>
         </div>
 
         <Footer />
